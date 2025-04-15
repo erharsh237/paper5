a0d57a41b3ac74fc1ba2a156145be9392af9f879
Harshpal Singh <erharsh237@gmail.com>
2025-04-16 01:35:53 +0530

    Made a navbar - but have to work on it more

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import './Navbar.css';
+import logoImg from "../assets/logo2.png"
+
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <header className="header">
+      <div className="logo">
+        <a href='index.html'>
+          <img src={logoImg} alt="Logo" />
+        </a>
+      </div>
+
+      <nav className={`nav ${isMenuOpen ? 'nav-open' : ''}`}>
+        <a href="#home" className="nav-link">Home</a>
+        <a href="#about" className="nav-link">About</a>
+        <a href="#contact" className="nav-link">Pricing</a>
+        <a href="#contact" className="nav-link">Contact</a>
+      </nav>
+
+      <div className="hamburger" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
